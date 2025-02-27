92ab693d334fe271d7a655237f5d6306754dddbb
Natalia López Gallego <166937483+ntlg72@users.noreply.github.com>
2025-02-26 23:40:25 -0500

    DB change to PostgreSQL

--- src/connection/db_utils.py
+++ src/connection/db_utils.py
@@ -1,16 +1,18 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pandas as pd
+import psycopg2
 
 def get_db_connection():
     load_dotenv()
-    user = os.getenv('MYSQL_USER')
-    password = os.getenv('MYSQL_PASSWORD')
-    host = os.getenv('MYSQL_HOST')
-    port = os.getenv('MYSQL_PORT')
-    dbname = os.getenv('MYSQL_DB')
-    db_url = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{dbname}"
+    user = os.getenv('PG_USER')
+    password = os.getenv('PG_PASSWORD')
+    host = os.getenv('PG_HOST')
+    port = os.getenv('PG_PORT')
+    dbname = os.getenv('PG_DATABASE')
+
+    db_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
 
     try:
         engine = create_engine(db_url)
@@ -20,7 +22,3 @@ def get_db_connection():
     except Exception as e:
         print(f"Error: {e}")
         return None
-
-def read_candidates_table(connection):
-    query = "SELECT * FROM candidates"
-    return pd.read_sql(query, connection)
