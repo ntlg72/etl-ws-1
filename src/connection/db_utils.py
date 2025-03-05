d3ae3d4f57a15aa25f64429f607b86448b11f373
Natalia López Gallego <166937483+ntlg72@users.noreply.github.com>
2025-03-05 14:57:05 -0500

    Update db_utils logic and notebook 1

--- src/connection/db_utils.py
+++ src/connection/db_utils.py
@@ -1,10 +1,8 @@
-import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, text
-import pandas as pd
-import psycopg2
+import os
+from sqlalchemy import create_engine
 
-def get_db_connection():
+def get_connection():
     load_dotenv()
     user = os.getenv('PG_USER')
     password = os.getenv('PG_PASSWORD')
@@ -12,13 +10,13 @@ def get_db_connection():
     port = os.getenv('PG_PORT')
     dbname = os.getenv('PG_DATABASE')
 
+    
     db_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}"
 
     try:
         engine = create_engine(db_url)
-        connection = engine.connect()
-        print("Connected to the database successfully")
-        return connection
+        print("Engine created succesfully")
+        return engine
     except Exception as e:
         print(f"Error: {e}")
         return None
